a525abc86fcbaf54926a36dd2726271ad90834ba
ajartijaouad <ajartijaouad14@gmail.com>
2025-12-03 17:02:34 +0100

    modfier le numero de tef de customer quia l id =1

--- db.sql
+++ db.sql
@@ -1,37 +1,48 @@
 CREATE DATABASE IF NOT EXISTS banklyy;
 USE banklyy;
-
-
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     full_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE,
     phone VARCHAR(12)
 );
-
-
 CREATE TABLE adviors (
     advisoe_id INT PRIMARY KEY AUTO_INCREMENT,
     full_nume VARCHAR(50),
     email VARCHAR(100)
 );
-
 CREATE TABLE accounts (
     account_id INT PRIMARY KEY AUTO_INCREMENT,
     account_number INT,
     balance VARCHAR(25),
-    account_type ENUM('checking','savinges','business'),
+    account_type ENUM('checking', 'savinges', 'business'),
     customerid INT,
     advisorid BIGINT
 );
 CREATE TABLE transactions (
     transaction_id INT PRIMARY KEY AUTO_INCREMENT,
     amount DECIMAL(10, 2),
-    transaction_type ENUM('debit','credit'),
+    transaction_type ENUM('debit', 'credit'),
     transaction_date DATE,
     account_id INT,
     FOREIGN KEY (account_id) REFERENCES accounts(account_id)
 );
+ALTER TABLE accounts ADD advisorid BIGINT;
+
+INSERT INTO customers (full_name, email, phone)
+VALUES ('farouk', 'farouk@gmail.com', '068794562'),
+('ahmed', 'ahmed@gmail.com', '0562556285'),
+('jamal', 'jamlo@gmail.com', '06558585885'),
+('farid', 'farid@gmail.com', '0568745222');
+INSERT INTO accounts (account_number, balance, account_type, customerid, advisorid)
+VALUES
+(10, '1054.50', 'business', 2, 11),
+(15, '1000.50', 'savinges', 5, 4),
+(16, '9555.20', 'checking', 5, 12),
+(15, '1000.50', 'savinges', 5, 12),
+(15, '1000.70', 'checking', 7, 8);
 
 
- INSERT INTO customers (full_name ,email ,phone) VALUES ('farouk','farouk@gmail.com','068794562'),('ahmed','ahmed@gmail.com','0562556285'),('jamal','jamlo@gmail.com','06558585885'),('farid','farid@gmail.com','0568745222');
+UPDATE customers
+SET phone = '0698765432'
+WHERE customer_id = 1;
